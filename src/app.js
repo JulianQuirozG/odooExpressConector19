4f0a77829f70cbb99a81a728be586ff5431bbf2e
DevMoveSas <iscalajul@gmail.com>
2025-09-30 11:16:34 -0500

    Purchase order, create, update, updaterows

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const billsRoutes = require('./routes/bills.routes');
 const attachmentsRoutes = require('./routes/attachments.routes');
 const journalRoutes = require('./routes/journal.routes');
 const quotationRoutes = require('./routes/quotation.routes');
+const purchaseOrderRoutes = require('./routes/purchasOrder.routes');
 const DbConfig = require('./config/db');
 
 const app = express();
@@ -29,6 +30,7 @@ app.use('/api/bills', billsRoutes);
 app.use('/api/attachments', attachmentsRoutes);
 app.use('/api/journal', journalRoutes);
 app.use('/api/quotation',quotationRoutes)
+app.use('/api/purchase-order',purchaseOrderRoutes)
 
 // Initialize the database connection
 /** 
