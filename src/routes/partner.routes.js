bc81accf88cda46c978e0afac7073f4d66337c1c
DevMoveSas <iscalajul@gmail.com>
2025-10-04 10:57:49 -0500

    create partner schema and data

--- src/routes/partner.routes.js
+++ src/routes/partner.routes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const partnerController = require('../controllers/partener.controller');
+const { validateBody } = require('../middleware/validateBody.middleware');
 const router = express.Router();
+const { clientSchema } = require('../schemas/client.schema');
 
     //Aqui van las rutas de partner
     router.get('/', partnerController.getPartners);
     router.get('/customers/', partnerController.getPartnersCustomers);
     router.get('/proveedores/', partnerController.getPartnersProveedores);
     router.get('/:id', partnerController.getOnePartner);
-    router.post('/', partnerController.createPartner);
+    router.post('/', validateBody(clientSchema), partnerController.createPartner);
     router.put('/:id', partnerController.updatePartner);   
     router.delete('/:id', partnerController.deletePartner);
-    router.post('/with-account', partnerController.createPartnerWithAccount);
+    router.post('/with-account', validateBody(clientSchema), partnerController.createPartnerWithAccount);
 
 module.exports = router;
